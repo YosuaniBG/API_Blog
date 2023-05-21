7a28087b0f9175d53c56d6786735dd27db52836c
YosuaniBG <yborgesg@gmail.com>
2023-05-21 11:10:43 -0700

    Actualizada consulta SQL de los Posts

--- models/post.model.js
+++ models/post.model.js
@@ -1,5 +1,5 @@
 const getAllPostsWithAuthor = () => {
-    return db.query('SELECT p.id_post, p.title, p.description, p.created_date, p.category , a.name as author, a.email, a.image FROM posts as p JOIN authors as a ON p.id_author = a.id_author');
+    return db.query('SELECT p.* , a.name as author, a.email, a.image FROM posts as p JOIN authors as a ON p.id_author = a.id_author');
 };
 
 //Consulta para CREAR un POST
